5769bbf84664747162c36ec217bd9245eb079bcf
jakobbnt <57390360+jakobbnt@users.noreply.github.com>
2021-05-30 18:59:45 +0200

    Indsat text og noget juice til dem

---
+++ Assets/Scripts/Text/TextStart.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextStart : MonoBehaviour
+{
+    Rigidbody2D ThisBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThisBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) ThisBody.bodyType = RigidbodyType2D.Dynamic;
+    }
+}
